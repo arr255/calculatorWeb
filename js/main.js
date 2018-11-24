433272f8eadca1de87d5a30bb25a24f2f4bcf25f
Maker <maker@qq.com>
2018-11-25 01:42:02 +0800

    lr direction

--- js/main.js
+++ js/main.js
@@ -1,31 +1,117 @@
 var myApp = new Framework7();
 //var mainView = myApp.addView('.view');
-var height=screen.height;
+var height=document.documentElement.clientHeight;
 var $$=Dom7;
 var buttonHeight=height*0.7/9;
-var currentFormula="$0$";
+var cursor="{\\color{yellow}|}"
+var initFormula="$"+cursor+"$"
+var currentFormula=initFormula;
 console.log(currentFormula);
 $$("a.button").css("height",buttonHeight+"px");
 $$("a").children().css("position","relative");
 $$("a").children().css("top",buttonHeight/3+"px");
 //增添
-function changeFormula(formula){
-    currentFormula=currentFormula.replace("$0","");//去除开头0
-    currentFormula=currentFormula.replace(/\$/g,"");
-    currentFormula=currentFormula+formula;
-    currentFormula="$"+currentFormula;
-    currentFormula=currentFormula+"$";
-    console.log(currentFormula);
-    $$("#formula").text(currentFormula);
-    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+function changeFormula(formula,leftMove=0,RightMove=0,boxed=0,curelyBrackets=0){
+    //删除光标
+    if(curelyBrackets){
+        currentFormula=currentFormula.Substring(0,currentFormula.Length-1);
+    }
+    //获取当前光标位置$xxxcursor
+    var cursorLocation=currentFormula.indexOf(cursor);
+    currentFormula=insertStr(currentFormula,cursorLocation,formula);
+    //添加末尾花括号(如果有需要)
+    if(curelyBrackets){
+        currentFormula=currentFormula+"}";
+    }
+    //重新显示
+    reload();
 }
-//AC按键
+/*
+    AC按键按下，清空内容，返回为0
+    返回：无
+*/
 function clearContent(){
-    $$("#formula").text("$0$");
-    currentFormula="$0$";
-    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    currentFormula="$"+cursor+"$";
+    reload();
 }
-
+/*
+    光标左移
+    参数：左移位数
+    返回：无
+*/
+function moveLeft(lm){
+    //获取当前光标位置$xxxcursor
+    var cursorLocation=currentFormula.indexOf(cursor);
+    if(cursorLocation>0){
+        currentFormula=currentFormula.replace(cursor,"");
+        //寻找合适的光标位置
+        //排除A-Z、a-z、/、[a-z]( 、[A-Z](
+        while(lm){
+            if(cursorLocation<2){
+                break;
+            }
+            testChar=currentFormula.charAt(cursorLocation-1);
+            if(cursorLocation>2){
+                testChar2=currentFormula.charAt(cursorLocation-2);
+            }
+            if(testChar.match(/\(/)){   
+                if(testChar2.match(/[a-z]/) || testChar2.match(/[A-Z]/)){
+                    cursorLocation-=1;
+                }
+            }
+            else if(testChar.match(/[a-z]/) || testChar.match(/[A-Z]/) || testChar.match(/\\/)){
+                cursorLocation-=1;
+            }
+            else{
+                cursorLocation-=1;
+                lm-=1;
+            }
+        }
+        currentFormula=insertStr(currentFormula,cursorLocation,cursor);
+        reload();
+    }
+}
+function moveRight(rm){
+    //获取当前光标位置$xxxcursor
+    var cursorLocation=currentFormula.indexOf(cursor);
+    if(cursorLocation+cursor.length+1<currentFormula.length){
+        currentFormula=currentFormula.replace(cursor,"");
+        while(rm){
+            if(cursorLocation+1<currentFormula.length){
+                testChar=currentFormula.charAt(cursorLocation);
+                testChar2=currentFormula.charAt(cursorLocation-1);
+                console.log("testChar:"+testChar);
+                if(testChar.match(/\(/)){   
+                    if(testChar2.match(/[a-z]/) || testChar2.match(/[A-Z]/)){
+                        cursorLocation+=1;
+                        rm-=1;
+                    }
+                }
+                else if(testChar.match(/[a-z]/) || testChar.match(/[A-Z]/) || testChar.match(/\\/)){
+                    cursorLocation+=1;
+                }
+                else{
+                    cursorLocation+=1;
+                    rm-=1;
+                }
+            }
+            else{
+                break;
+            }
+        }
+        currentFormula=insertStr(currentFormula,cursorLocation,cursor);
+        reload();
+    }
+}
+/*
+    insertStr(str,location,insertContent)
+    参数：str：待处理字符串；location：插入的位置;insertContent:插入的内容
+    返回值：处理后的字符串
+*/
+function insertStr(str,location,insertContent){
+    str=str.substr(0,location)+insertContent+str.substr(location,str.length);
+    return str;
+}
 /*******************************
  * 计算部分
  ******************************/
@@ -45,9 +131,22 @@ function calculate(string){
     console.log(string);
    $$("#result").text(ApproResult);
 }
-function handleString(s){
-    var str=s.replace(/\$/g,"");
+function handleString(str){
+    //删除光标
+    str=str.replace(cursor,"");
+    str=str.replace(/\$/g,"");
+    str=str.replace(/\\times/g,"*");
+    str=str.replace(/\\div/,"/");
+    str=str.replace(/\\sin/,"math.sin");
+    str=str.replace(/\\cos/,"math.cos");
+    str=str.replace(/\\tan/,"math.tan");
+    str=str.replace(/\\pi/,math.PI);
     console.log("handledString:"+str);
     return str;
+}
+function reload(){
+    $$("#formula").text(currentFormula);
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    console.log("currentFormula:"+currentFormula);
 }
 
